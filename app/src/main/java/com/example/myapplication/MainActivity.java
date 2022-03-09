a6a03a9ba01785ac83dbcd5c30b55412d95d5f78
samezzzz228 <rasulov_aziret>
2022-03-09 18:37:40 +0600

    Init

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -3,53 +3,84 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.example.myapplication.databinding.ActivityMainBinding;
+
+
 public class MainActivity extends AppCompatActivity {
-private Button rt;
-private TextView rr;
-private EditText rrr, tt;
+    private ActivityMainBinding binding ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        rt = findViewById(R.id.rt);
-        rr = findViewById(R.id.rr);
-        rrr = findViewById(R.id.rrr);
-        tt = findViewById(R.id.tt);
-
-        rt.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                if (rrr.getVisibility() == View.VISIBLE) {
-                    rrr.setVisibility(View.GONE);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        initListener();
+        editListener();
+    }
+    private void initListener() {
+        binding.rt.setEnabled(false);
+        binding.rt.setOnClickListener(view -> {
+            String login = binding.rrr.getText().toString().trim();
+            String password = binding.tt.getText().toString().trim();
+            if (login.equals("Admin") && password.equals("admin@12345")) {
+                Toast.makeText(this, "Вы зарегистрировались",
+                        Toast.LENGTH_LONG).show();
+                if (binding.rrr.getVisibility() == View.VISIBLE) {
+                    binding.rrr.setVisibility(View.GONE);
                 } else {
-                    rrr.setVisibility(View.VISIBLE);
+                    binding.rrr.setVisibility(View.VISIBLE);
                 }
-                if (tt.getVisibility() == View.VISIBLE) {
-                    tt.setVisibility(View.GONE);
+                if (binding.tt.getVisibility() == View.VISIBLE) {
+                    binding.tt.setVisibility(View.GONE);
                 } else {
-                    tt.setVisibility(View.VISIBLE);
+                    binding.tt.setVisibility(View.VISIBLE);
                 }
-                if (rr.getVisibility() == View.VISIBLE) {
-                    rr.setVisibility(View.GONE);
+                if (binding.rr.getVisibility() == View.VISIBLE) {
+                    binding.rr.setVisibility(View.GONE);
                 } else {
-                    rr.setVisibility(View.VISIBLE);
+                    binding.rr.setVisibility(View.VISIBLE);
                 }
-                Toast toast = new Toast(MainActivity.this);
-                String text = rrr.getText().toString();
-                if (text.isEmpty()) {
-                    toast.setText("Введите данные");
-                } else {
-                    toast.setText("Вы успешно зарегистрировались");
-                }
-                toast.show();
+            } else {
+                Toast.makeText(this, "Неправильный логин или пороль",
+                        Toast.LENGTH_LONG).show();
             }
         });
+        binding.txtForget.setOnClickListener(view ->
+                Toast.makeText(this, "Ваш логин : Admin \n Ваш пароль : admin@12345 "
+                        , Toast.LENGTH_LONG).show());
+    }
+    public void editListener() {
+        TextWatcher textWatcher = new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                if (binding.rrr.getText().length() > 0 && binding.tt.getText().length() > 0) {
+                    binding.rt.setBackgroundResource(R.drawable.yy);
+                    binding.rt.setEnabled(true);
+                } else {
+                    binding.rt.setBackgroundResource(R.drawable.rr);
+                    binding.rt.setEnabled(false);
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+
+            }
+        };
+        binding.rrr.addTextChangedListener(textWatcher);
+        binding.tt.addTextChangedListener(textWatcher);
     }
 }
